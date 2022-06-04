0952ff1c80aa3d806f78b36b11d19392b5cddcf6
Mini Bhati <minibhati93@gmail.com>
2022-06-04 19:59:19 +0530

    Bootstrap Standalone Component

---
+++ src/components/root/root.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.scss'],
+})
+export class RootComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
